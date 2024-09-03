fc602c3a42c6776888d7d6bb24876f6182176696
윤병서 <bsyun0571@naver.com>
2024-09-03 17:48:16 +0900

    feat: 240903 learning

--- onebite-next/section02/src/pages/_app.tsx
+++ onebite-next/section02/src/pages/_app.tsx
@@ -2,5 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <header>헤더</header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>푸터</footer>
+    </div>
+  );
 }
